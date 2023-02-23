4dbe79e02de88541efa093212a56113572c8ecdb
magguer <martintelaguer@gmail.com>
2023-02-23 17:57:11 -0300

    followings-followers-commit

--- Twitter/controllers/userController.js
+++ Twitter/controllers/userController.js
@@ -2,16 +2,21 @@ const User = require("../models/User");
 
 async function followers(req, res) {
   const userFollowers = await User.findOne({ username: req.params.username })
-  return res.render("pages/followers", { userFollowers })
+  const followers = userFollowers.followers
+  const users = await User.find({ _id: { $in: followers } })
+  return res.render("pages/followers", { users })
 }
 
 async function following(req, res) {
   const userFollowing = await User.findOne({ username: req.params.username })
-  return res.render("pages/following", { userFollowing })
+  const followings = userFollowing.following
+  const users = await User.find({ _id: { $in: followings } })
+  return res.render("pages/following", { users })
 }
 
 async function profile(req, res) {
-  return res.render("pages/profile")
+  const userProfile = await User.findById(req.params.id)
+  return res.render("pages/profile", { userProfile })
 }
 
 module.exports = {
