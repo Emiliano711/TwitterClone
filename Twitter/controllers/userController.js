58b30b1ef1b24c3709a15044aabea4e8f2f04676
magguer <martintelaguer@gmail.com>
2023-02-23 17:16:07 -0300

    followers-following-aded

--- Twitter/controllers/userController.js
+++ Twitter/controllers/userController.js
@@ -1,13 +1,13 @@
 const User = require("../models/User");
 
 async function followers(req, res) {
-  const users = await User.find()
-  return res.render("pages/followers", { users })
+  const userFollowers = await User.findOne({ username: req.params.username })
+  return res.render("pages/followers", { userFollowers })
 }
 
 async function following(req, res) {
-  const users = await User.find()
-  return res.render("pages/following", { users })
+  const userFollowing = await User.findOne({ username: req.params.username })
+  return res.render("pages/following", { userFollowing })
 }
 
 async function profile(req, res) {
